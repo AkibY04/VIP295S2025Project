27768d0b382a04755d36d7ae3053521d635c5fbd
Akib Yeahia <akib.yeahia@stonybrook.edu>
2025-02-10 17:16:16 -0500

    Reconfigured repo

--- src/index.js
+++
@@ -1,9 +0,0 @@
-// ********************* DO NOT CHANGE THIS FILE *********************
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App />
-);
