4dde6e6ff03bfa3793ed955c312e52bb8fed78c8
Layton <layton.madeira@stonybrook.edu>
2025-02-16 16:02:59 -0500

    further api testing w/ functions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 window.onload = async function() {
     console.log("Script loaded!");
-    let response = await fetch("https://api.fda.gov/drug/event.json?limit=5");
+    let response = await fetch("https://api.fda.gov/drug/event.json?search=receivedate:[20040101+TO+20081231]sort=receivedate:desc&limit=10");
     const json = await response.json();
 
     for(let i = 0; i < json.results.length; i++){
@@ -21,5 +21,29 @@ window.onload = async function() {
         console.log(`Drug Indication:`, drug.drugindication);
         });
     }
-    // console.log(json);
+
+    console.log("=============TESTING=============");
+
+    console.log(json);
+
+    let resp = await searchQuery(10, 1, 20240101, 20241231);
+    resp = await resp.json();
+    console.log(resp);
 };
+
+async function searchQuery(limit, order, dateStart, dateEnd){
+    
+    let searchRange = "";
+    if(order==0){
+        order='desc'
+    }
+    else{
+        order = 'asc'
+    }
+    if(dateStart && dateEnd){
+        searchRange = `search=receivedate:[${dateStart}+TO+${dateEnd}]`
+    }
+
+    let response = await fetch(`https://api.fda.gov/drug/event.json?${searchRange}&sort=receivedate:${order}&limit=${limit}`);
+    return response;
+}
