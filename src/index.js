c3deeb3e1b2754928684c1840082556ed8dc1cc9
Akib Yeahia <akib.yeahia@stonybrook.edu>
2025-02-19 17:14:29 -0500

    Small tweaks

--- src/index.js
+++ src/index.js
@@ -4,18 +4,18 @@ window.onload = async function() {
     const json = await response.json();
 
     for(let i = 0; i < json.results.length; i++){
-        // // Access patient information
+        //Access patient information
         console.log("==========");
         const patient = json.results[i].patient;
         console.log("Patient Onset Age:", patient.patientonsetage);
         console.log("Patient Sex:", patient.patientsex);
 
-        // Access reactions
+        //Access reactions
         patient.reaction.forEach((reaction, index) => {
         console.log(`Reaction ${index + 1}:`, reaction.reactionmeddrapt);
         });
 
-        // Access drug information
+        //Access drug information
         patient.drug.forEach((drug, index) => {
         console.log(`Drug ${index + 1} - Medicinal Product:`, drug.medicinalproduct);
         console.log(`Drug Indication:`, drug.drugindication);
