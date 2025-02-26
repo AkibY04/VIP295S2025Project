17aecf1f309d0fe6b004b7b856aee1782912e42e
Akib Yeahia <akib.yeahia@stonybrook.edu>
2025-02-25 19:20:19 -0500

    small tweaks

--- src/index.js
+++ src/index.js
@@ -1,64 +1,33 @@
 var map = {};
 window.onload = async function() {
     console.log("Script loaded!");
-    let response = await fetch(     "https://api.fda.gov/drug/event.json?search=receivedate:[20040101+TO+20081231]&sort=receivedate:desc&limit=10");
-    const json = await response.json();
-
-    // for(let i = 0; i < json.results.length; i++){
-    //     // // Access patient information
-    //     console.log("==========");
-    //     const patient = json.results[i].patient;
-    //     console.log("Patient Onset Age:", patient.patientonsetage);
-    //     console.log("Patient Sex:", patient.patientsex);
-
-    //     // Access reactions
-    //     patient.reaction.forEach((reaction, index) => {
-    //     console.log(`Reaction ${index + 1}:`, reaction.reactionmeddrapt);
-    //     });
-
-    //     // Access drug information
-    //     patient.drug.forEach((drug, index) => {
-    //     console.log(`Drug ${index + 1} - Medicinal Product:`, drug.medicinalproduct);
-    //     console.log(`Drug Indication:`, drug.drugindication);
-    //     });
-    // }
-
-    console.log("=============TESTING=============");
-    console.log(json);
 
     let resp = await searchQuery(10, 1, 20240101, 20241231);
     resp = await resp.json();
     console.log(resp);
 
     console.log("=============TESTING=============\n\n\n\n\n\n\n\n\n");
-
-    await aggregateData();
-    
+    await aggregateData(20240101, 20241231, 'desc', 1000, 2);
     console.log(map);
 
 };
 
-async function aggregateData(){
-    let dateStart   = "20240101";
-    let dateEnd     = "20241231";
+async function aggregateData(dateStart, dateEnd, order, limit, batches){
     let searchRange = `search=receivedate:[${dateStart}+TO+${dateEnd}]`;
-    let order       = 'desc';
-    let limit       = '1000';
-    let batches     =  2;
-    // 2 batches
     for(let i = 0; i < batches; i++){
 
         let response = await fetch(`https://api.fda.gov/drug/event.json?${searchRange}&sort=receivedate:${order}&limit=${limit}`);
+
         const json = await response.json();
         for(let i = 0; i < json.results.length; i++){
-            // // Access patient information
+            //Access patient information
             console.log("==========");
             const patient = json.results[i].patient;
     
-            // Access drug information
+            //Access drug information
             patient.drug.forEach((drug, index) => {
-                // console.log(`Drug ${index + 1} - Medicinal Product:`, drug.medicinalproduct);
-                // console.log(`Drug Indication:`, drug.drugindication);
+                //console.log(`Drug ${index + 1} - Medicinal Product:`, drug.medicinalproduct);
+                //console.log(`Drug Indication:`, drug.drugindication);
                 if (drug.medicinalproduct in map){
                     map[drug.medicinalproduct] = map[drug.medicinalproduct]+1;
                 }
